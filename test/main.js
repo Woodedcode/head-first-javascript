ed016ee3125307f2c945f2bd9045fc8e884dd49a
Ryan <98045919+Woodedcode@users.noreply.github.com>
2024-06-13 10:34:59 -0500

    Update main.js

--- test/main.js
+++ test/main.js
@@ -56,16 +56,30 @@
 
 
 
-var name = "Joe"
-var i = 0
+// var name = "Joe"
+// var i = 0
 
 
-while (i < 2) {
-    document.write("Happy Birthday to you! <br>")
-    i = i + 1
-}
-document.write("Happy Birthday dear " + name + ",<br>")
-document.write("Happy Birthday to you! <br>")
+// while (i < 2) {
+//     document.write("Happy Birthday to you! <br>")
+//     i = i + 1
+// }
+// document.write("Happy Birthday dear " + name + ",<br>")
+// document.write("Happy Birthday to you! <br>")
 
-var message = "Howdy" + ' ' + "Partner"
-console.log(message)
+// var message = "Howdy" + ' ' + "Partner"
+// console.log(message)
+
+// var word = "bottles";
+// var count = 99;
+
+// while (count > 0) {
+//     console.timeLog(count + " " + word + " of beer,");
+//     console.log("Take one down, pass it around,")
+//     count = count - 1
+//     if (count > 0) {
+//         console.log(count + ' ' + word + ' of beer on the wall.')
+//     } else {
+//         console.log(" No more " + word + " of beer on the wall")
+//     }
+// }
