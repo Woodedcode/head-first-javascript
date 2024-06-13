c048e1ab899d0dff04a8663de18b6244b9b58b44
Ryan <98045919+Woodedcode@users.noreply.github.com>
2024-06-13 10:28:12 -0500

    Update main.js

--- test/main.js
+++ test/main.js
@@ -1,4 +1,71 @@
-setTimeout(wakeUpUser, 5000);
-function wakeUpUser() {
-    alert("Are you goung to stare at this boring page forever?")
+// setTimeout(wakeUpUser, 5000);
+// function wakeUpUser() {
+//     alert("Are you goung to stare at this boring page forever?")
+// }
+
+
+//test for jokes
+// var joke = "JavaScript walked into a bar....";
+// var toldJoke = false;
+// var $punchline = 
+// "Better watch out for those semi-colons."
+// var percentage = 20;
+// var result
+
+// if (toldJoke == true) {
+//     alert($punchline);
+// } else {
+//     alert(joke)
+// }
+
+//Movie Night
+// var zipCode = 98104;
+// var joesFavoriteMovie = 'Forbidden Planet';
+// var movieTicket$ = 9;
+// if (movieTicket$ >= 9) {
+//     alert('Too Much!')
+// } else {
+//     alert("We're going to see " + joesFavoriteMovie)
+// }
+
+// page 16 sharpen your pencil
+// (9/5) * temp + 32
+
+//page 17
+// var scoops = 5
+// while (scoops > 0) {
+//     document.write('Another Scoop!<br>')
+//     scoops = scoops - 1
+// }
+// document.write("Life without ice cream isn't the same!")
+// let scoops = 5
+
+// if (scoops >= 5) {
+//     alert("Eat faster, the ice cream is going to melt!")
+// } else if (scoops === 3) {
+//     alert("Ice Cream is running low!")
+// } else if (scoops === 2) {
+//     alert("Going once!")
+// } else if (scoops === 1) {
+//     alert("Going twice!")
+// } else if (scoops === 0) {
+//     alert("Gone!")
+// } else {
+//     alert("Still lots of ice cream left, come get it!")
+// }
+
+
+
+var name = "Joe"
+var i = 0
+
+
+while (i < 2) {
+    document.write("Happy Birthday to you! <br>")
+    i = i + 1
 }
+document.write("Happy Birthday dear " + name + ",<br>")
+document.write("Happy Birthday to you! <br>")
+
+var message = "Howdy" + ' ' + "Partner"
+console.log(message)
